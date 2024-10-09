a250edec9b00628bd4db5449a9ea93d5a82c4d9f
MBORI BRANDON <151223516+Edenchris10@users.noreply.github.com>
2024-10-09 08:27:47 +0200

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -18,7 +18,7 @@ const Photo = () => {
              }}
         className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
         <Image 
-            src="/assets/pika.png" 
+            src="/assets/Pika.png" 
             priority 
             quality={100} fill 
             alt="" 
